d8041e419acc1d6a60b09d968607500947477f82
AleksaMCode <aleksamcode@gmail.com>
2022-07-27 11:59:28 +0200

    api call unit metric added

--- weatherapp/src/app/services/weather.service.ts
+++ weatherapp/src/app/services/weather.service.ts
@@ -9,7 +9,7 @@ export class WeatherService {
 
   getCurrentWeatherForCity(city: string): Observable<any>
   {
-    const path = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=695ed9f29c4599b7544d0db5c211d499`
+    const path = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=695ed9f29c4599b7544d0db5c211d499&units=metric`
     
     return this.http.get(path);
   }
